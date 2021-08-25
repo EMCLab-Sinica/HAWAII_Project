2bc941b62c1ee4712e0ef8f7c79162c99fd0c504
A-Kai Kang <akaikang@users.noreply.github.com>
2021-08-25 22:55:55 +0800

    Add files via upload
    bug fix

--- main.c
+++ main.c
@@ -11,6 +11,9 @@ int main(void)
 	msp_lea_init();
 	while(1){
 		HAW_INFERENCE(&network);
+
+		_DBGUART("\r\n result : %d %d %d %d %d %d %d %d %d %d \r\n",result[0],result[1],result[2],result[3],result[4],
+																    result[5],result[6],result[7],result[8],result[9]);
 		P1OUT^=0x1;
 	}
 	
